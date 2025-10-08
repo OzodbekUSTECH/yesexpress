4469cae68410e1dbb73e526674670d239ba4ba19
OzodbekUSTECH <naimovozod81@gmail.com>
2025-10-08 13:24:00 +0500

    fixed erros

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,9 @@ echo "Apply database migrations"
 uv run manage.py migrate
 
 # Start collect staticfiles
-echo  "Start collect static"
+echo "Start collect static"
 uv run manage.py collectstatic --noinput
 
-
-# Start Uvicorn processes
-echo "Starting Operator Gunicorn."
-exec uv run gunicorn tuktuk.wsgi:application --bind 0.0.0.0:4546 --workers 2 --log-level debug
+# Start Gunicorn processes
+echo "Starting Gunicorn server."
+exec uv run gunicorn tuktuk.wsgi:application --bind 0.0.0.0:4546 --workers 2 --log-level info --access-logfile - --error-logfile -
